45a60b1c8df74bd65cc4a76ddf6f18bf426af092
giulio <giulio@MacBook-Pro-di-biopixel.local>
2026-04-08 00:11:50 +0200

    homebrew: v1.2.3 sha256 (arm64 + x86_64)

--- homebrew/youtube-blocker.rb
+++ homebrew/youtube-blocker.rb
@@ -2,12 +2,12 @@ cask "youtube-blocker" do
   version "1.2.3"
 
   on_arm do
-    sha256 "REPLACE_WITH_ARM64_SHA256"
+    sha256 "39b2651500865fa5159cd95e6aa8f1fef7e3b9e7d5a94cb79d9f6fa6dd53d845"
     url "https://github.com/zoott28354/Youtube-Blocker/releases/download/v#{version}/YouTubeBlocker_#{version}_aarch64.dmg"
   end
 
   on_intel do
-    sha256 "REPLACE_WITH_X64_SHA256"
+    sha256 "24bbe10f5dc4a2883793797e155bc31cb7d0220053b41e104a28a86447773fc5"
     url "https://github.com/zoott28354/Youtube-Blocker/releases/download/v#{version}/YouTubeBlocker_#{version}_x64.dmg"
   end
 
